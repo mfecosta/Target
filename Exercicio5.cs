e6cd7b84ed2a1682b6f5c87d229b442a682399c3
mfecosta <mfecosta11@hotmail.com>
2023-04-24 21:34:44 -0300

    Exercicios

---
+++ Exercicio5.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TargetTest
+{
+    class Exercicio5
+    {
+        public void InverterString()
+        {
+            Console.Write("Qual a frase a ser invertida: ");
+            string texto = Console.ReadLine();
+
+            // Convertendo a string em um array de caracteres
+            char[] caracteres = texto.ToCharArray();
+
+            // Invertendo a ordem
+            for (int i = 0; i < caracteres.Length / 2; i++)
+            {
+                char temp = caracteres[i];
+                caracteres[i] = caracteres[caracteres.Length - 1 - i];
+                caracteres[caracteres.Length - 1 - i] = temp;
+            }
+
+            // Convertendo o array de caracteres de volta para uma string
+            string textoInvertido = new string(caracteres);
+
+            
+            Console.WriteLine("String invertida: " + textoInvertido);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace TargetTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Exercicio1 exercicio1 = new Exercicio1();
+            Exercicio2 exercicio2 = new Exercicio2();
+            Exercicio3 exercicio3 = new Exercicio3();
+            Exercicio4 exercicio4 = new Exercicio4();
+            Exercicio5 exercicio5 = new Exercicio5();
+            
+            
+
+            Console.WriteLine("Qual exercício você irá executar ?");
+            Console.WriteLine("Exercício - 1\n Exercício - 2\n Exercício - 3\n Exercício - 4\n Exercício - 5");
+            int exercicio = int.Parse(Console.ReadLine());
+            
+            switch (exercicio)
+            {
+                
+                case 1: exercicio1.ValorSoma();
+                    break;
+                case 2: exercicio2.PertenceFibonacci();
+                    break;
+                case 3: exercicio3.CalcularFaturamento();                  ;
+                    break;
+                case 4:
+                    exercicio4.FaturamentoPercentual(); ;
+                    break;
+                case 5:
+                    exercicio5.InverterString(); ;
+                    break;
+                default: Console.WriteLine("Escolha um valor na lista");
+                    break;
+            }
+            
+            
+        }
+    }
+}
